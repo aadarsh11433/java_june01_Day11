d372706151f9a8f4e86c37c3b5a215b8eff28933
Aadarsh Mukati <aadarsh11433@gmail.com>
2022-06-12 11:27:08 +0530

    test commit

---
+++ Notes/Color.java
@@ -0,0 +1,32 @@
+package com.Notes;
+
+public class Color{
+	
+	enum Colour {
+		RED,
+		GREEN,
+		BLUE;
+	}
+		public static void main(String[] args)
+
+		{
+
+			Colour[] c1 = Colour.values();
+
+			for (Colour col : c1) {
+				// Calling ordinal() to find index of color
+				System.out.println(col + " at index " + col.ordinal());
+				}
+
+		}
+	
+}
+	
+	
+		
+		
+	
+
+	
+
+
